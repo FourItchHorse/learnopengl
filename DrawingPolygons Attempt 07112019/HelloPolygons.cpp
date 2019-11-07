f9539db237664bfceef774b11af4445f611babb0
Joseph Riley <joerileyart@gmail.com>
2019-11-07 18:03:42 +0000

    Created file

--- DrawingPolygons Attempt 07112019/HelloPolygons.cpp
+++ DrawingPolygons Attempt 07112019/HelloPolygons.cpp
@@ -17,7 +17,7 @@ const char* vertexShaderSource = "#version 330\n"
 const char* fragmentShaderSource = "#version 330\n"
 "out vec4 FragColor;"
 "void main() {"
-" FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+" FragColor = vec4(0.5f, 0.5f, 0.5f, 1.0f);"
 "}";
 
 int main () 
