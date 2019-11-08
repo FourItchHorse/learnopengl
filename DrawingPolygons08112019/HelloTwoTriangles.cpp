540280ee49a1b3d16b5bee6087ddc7790a87a637
Joseph Riley <joerileyart@gmail.com>
2019-11-08 16:12:15 +0000

    Apparently changed 05112019 file

--- DrawingPolygons08112019/HelloTwoTriangles.cpp
+++ DrawingPolygons08112019/HelloTwoTriangles.cpp
@@ -1,5 +1,5 @@
 #include <D:/source/include/glad.h> //starting from scratch without copying to revise concepts
 #include <D:/source/include/glfw3.h>
 
-//goal: create two triangles, one blue one yellow with different VAOs and VBOS
+//goal: create two triangles, one blue one yellow with different VAOs and VBOS (and two different fragment shaders)
 
