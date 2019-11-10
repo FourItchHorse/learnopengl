35998c31134b71c45f30159015b45389c656c000
Joseph Riley <joerileyart@gmail.com>
2019-11-10 15:54:42 +0000

    Found out I was getting error 1281/501
    Managed to solve the error by correcting the
    count of VertexAttribPointer. Then managed
    to implement EBOs and get the 6 vertex star rendered.
    Packt LearnOpenGL book just seems to have the similar code
    to learnopengl.com since both use GLFW.

--- 10112019/SDLandGLEWPolygons.cpp
+++ 10112019/SDLandGLEWPolygons.cpp
@@ -5,18 +5,18 @@
 #include<D:/source/include/SDL_opengl.h>
 #include <iostream>
 
-const GLchar* vertexShader2DSource = R"glsl(#version 330 core
-in vec2 position;
+const GLchar* vertexShaderSource = R"glsl(#version 330 core 
+layout (location = 0) in vec3 position;
 void main()
 {
-gl_position = vec4(position, 0, 1.0);
+gl_Position = vec4(position.x, position.y, position.z, 1.0);
 })glsl";
 
-const GLchar* fragmentShaderSource = R"glsl(#version 330  core
-out vec4 outColor;
+const GLchar* fragmentShaderSource = R"glsl(#version 330 core
+out vec4 color;
 void main()
 {
-    outColor = vec4(1.0,1.0,1.0,1.0);
+color = vec4(1.0,1.0,1.0,1.0);
  })glsl";
 
 int main (int argc, char *argv[]) 
@@ -27,49 +27,22 @@ int main (int argc, char *argv[])
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
-    SDL_Window* window = SDL_CreateWindow("Polygons with OpenGL with SDL", 200, 100, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_Window* window = SDL_CreateWindow("Polygons with GLEW and SDL", 200, 100, 800, 600, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(window); 
     
     glewExperimental = GL_TRUE;
     glewInit();
 
-    
-    GLuint  vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-
-    GLfloat vertices[] {
-        -0.05,1.0,  //top corner
-        -0.6,0.25, //upper left corner
-        0.6, 0.35,  //upper right corner
-        //-0.45,-0.7,  //lower left corner
-        //0.45, -0.7,  //lower right corner
-        //0.2, 0.0,  //pinch point betweeen upper right and lower left corner
-    };
-    
-
-    /*GLuint indices[] {
-     0, 3, 5, 
-     2, 1, 5,
-     4, 1, 5
-    };*/
-    
-    
-    glBindBuffer(GL_ARRAY_BUFFER, vbo); //creating and setting vbo as the active buffer
-    
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //copying vertices to vbo
-    
-    GLuint vertexShader2D = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader2D, 1 , &vertexShader2DSource, NULL);
-    glCompileShader(vertexShader2D);
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1 , &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
     GLint status;
     GLchar buffer[512];
-    glGetShaderiv(vertexShader2D, GL_COMPILE_STATUS, &status);
-    glGetShaderInfoLog(vertexShader2D, 512, NULL, buffer);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+    glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
+
+    std::cout<<"Compiling vertex shader...\n" << buffer;
 
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -78,22 +51,56 @@ int main (int argc, char *argv[])
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
     glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
 
+    std::cout<<"Compiling fragment shader...\n" << buffer;
+
     GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader2D);
+    glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
-    glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glBindFragDataLocation(shaderProgram, 0, "color");
     glLinkProgram(shaderProgram);
 
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
     glGetProgramInfoLog(shaderProgram, 512, NULL, buffer);
+    
+    std::cout<< "Linking shaders to program...\n" << buffer;
 
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    GLuint vao, vbo, ebo;
+    glGenBuffers(1, &ebo);
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
 
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
-   
-    const char* myString = static_cast<const char*>(glGetError());
-    printf();
+    GLfloat vertices[] {
+        -0.1,1.0, 0, //top corner
+        -0.7,0.25, 0,//upper left corner
+        0.6, 0.35,  0, //upper right corner
+        -0.45,-0.7, 0,  //lower left corner
+        0.45, -0.7, 0, //lower right corner
+        0.2, -0.1, 0, //pinch point betweeen upper right and lower left corner
+    };
+    
+
+    GLuint elements[] {
+     0, 3, 5, 
+     2, 1, 5,
+     4, 1, 5
+    };
+    
+    glBindVertexArray(vao);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vbo); //creating and setting vbo as the active buffer    
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //copying vertices to vbo
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*) 0);
+    glEnableVertexAttribArray(0);
+
+    printf("OpenGL error: %u\n", glGetError());
 
     SDL_Event windowEvent;
     while(true) 
@@ -110,15 +117,14 @@ int main (int argc, char *argv[])
 
         
         glUseProgram(shaderProgram);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         SDL_GL_SwapWindow(window);
     }
 
-    glDeleteProgram(shaderProgram);
-    glDeleteShader(vertexShader2D);
-    glDeleteShader(fragmentShader);
 
     glDeleteBuffers(1, &vbo);
     
