f378f42fa00e7bf926049ff00e291de44b49da61
Joseph Riley <joerileyart@gmail.com>
2019-11-10 12:46:27 +0000

    Finally managed to create a window using SDL and glew without a segmentation fault thanks to stack overflow

---
+++ 10112019/ShaderIntroduction.cpp
@@ -0,0 +1,47 @@
+#define GLEW_STATIC
+#include<D:/source/include/glew.h>
+#define SDL_MAIN_HANDLED
+#include<D:/source/include/SDL.h>
+#include<D:/source/include/SDL_opengl.h>
+#include <iostream>
+
+
+int main (int argc, char *argv[]) 
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    SDL_Window* window = SDL_CreateWindow("OpenGL with SDL", 200, 100, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    
+    glewExperimental = GL_TRUE;
+    glewInit();
+    
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+
+
+    printf("%u\n", vertexBuffer);
+
+    
+    SDL_Event windowEvent;
+    while(true) 
+    {   
+        if(SDL_PollEvent(&windowEvent)) 
+        {
+            if(windowEvent.type == SDL_QUIT) { break; }
+            if (windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE){  break; }
+            
+        }
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_GL_DeleteContext(context);
+    SDL_Quit();
+    return 0;
+}
+
