b03db88fb72d586fb5cb4f7aa457fe5b5927566c
Joseph Riley <joerileyart@gmail.com>
2019-11-12 13:42:25 +0000

    Thought I couldn't implement a function but was just reading a 1 as an l

--- 1112112019/Shader.h
+++ 1112112019/Shader.h
@@ -8,6 +8,8 @@
 #include <sstream>
 #include <iostream>
 
+
+
 class Shader 
 {
     public:
@@ -38,9 +40,57 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
         vShaderFile.close();
         fShaderFile.close();
         vertexCode = vShaderStream.str();
-        fragmentCode = fShaderStream.str();
+        fragmentCode= fShaderStream.str();
     }
- 
-}
+    catch (std::fstream::failure e)
+    {   
+        std::cout<<"ERROR:SHADER FILE NOT SUCCESSFULLY READ"<<std::endl;
+    }
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str(); 
+    unsigned int vertex, fragment;
+    int status;
+    char buffer[512];
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    glGetShaderInfoLog(vertex, GL_COMPILE_STATUS, &status, buffer);
+    std::cout << "Compiling vertex shader...\n" << buffer;
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glGetShaderInfoLog(vertex, GL_COMPILE_STATUS, &status, buffer);
+    std::cout << "Compiling fragment shader...\n" << buffer;
+
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+    glGetProgramInfoLog(ID, GL_LINK_STATUS, &status, buffer);
+    std::cout << "Linking shader program...\n" << buffer;
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    }
+
+    void Shader::use() 
+    {
+        glUseProgram(ID);
+    }
+
+    void Shader::setBool(const std::string name, bool value) 
+    {
+        glGetUniformLocation();
+    }
+    void Shader::setInt(const std::string name, int value) 
+    {
+
+    }
+    void Shader::setFloat(const std::string name, float value) 
+    {
+
+    }
 
 #endif
