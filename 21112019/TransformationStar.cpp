b92a87e3c6dabdcbfe4374690df2f295f246a7a1
Joseph Riley <joerileyart@gmail.com>
2019-11-21 18:11:25 +0000

    Managed to create a rotating star
    along with a view and projection matrix. I still have some way to
    go with understanding texture coordinates, but my element buffer knowledge
    seems fine.

--- 21112019/TransformationStar.cpp
+++ 21112019/TransformationStar.cpp
@@ -1,158 +1,166 @@
-#define GLEW_STATIC
-#include <GL/glew.h>
 
+#define GLEW_STATIC
 #define SDL_MAIN_HANDLED
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <GL\glew.h>
+
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
+#include <SDL2\SDL_opengl.h>
+
+#include <STB\stb_image.h>
 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include <glm\gtc\matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <STB/stb_image.h>
 
 #include <iostream>
 
-//create vertex and fragment shader source code
 const GLchar* vertexShaderSource = R"glsl(
-    #version 330 core
-    in vec2 position;
-    in vec3 color;
-    in vec2 texcoord1;
-    in vec2 texcoord2;
-    out vec3 Color;
-    out vec2 Texcoord1;
-    out vec2 Texcoord2;
-    uniform mat4 transform;
+#version 330 core
+in vec2 position;
+in vec3 color;
+in vec2 texcoord1;
+in vec2 texcoord2;
 
-    void main() 
-    {
-        Color = color;
-        Texcoord1 = texcoord1;
-        Texcoord2 = texcoord2;
+out vec3 Color;
+out vec2 TexCoord1;
+out vec2 TexCoord2;
 
-        gl_Position = transform * vec4(position.x, position.y, 0.0, 1.0);
-    }
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
 
+void main () 
+{
+    Color = color;
+    TexCoord1 = texcoord1;
+    TexCoord2 = texcoord2;
+    
+    gl_Position = proj * view * model * vec4(position, 0.0, 1.0);
+}
 )glsl";
 
 const GLchar* fragmentShaderSource = R"glsl(
-    #version 330 core
-    uniform sampler2D tex1;
-    uniform sampler2D tex2;
-    uniform float texWeight;
-    uniform mat4 transform;
-    in vec3 Color;
-    in vec2 Texcoord1;
-    in vec2 Texcoord2;
-    out vec4 outColor;
+#version 330 core
+in vec3 Color;
+in vec2 TexCoord1;
+in vec2 TexCoord2;
 
-    void main() 
-    {
-        vec4 colTex1 = texture(tex1, Texcoord1);
-        vec4 colTex2 = texture(tex2, Texcoord2);
-        outColor = mix(colTex1, colTex2, 0.5) * (transform * vec4(Color, 1));
-    }
+out vec4 outColor;
 
+
+uniform float time;
+uniform mat4 colorEffect;
+
+uniform sampler2D tex1;
+uniform sampler2D tex2;
+
+void main () 
+{
+    vec4 colTex1 = texture(tex1, TexCoord1);
+    vec4 colTex2 = texture(tex2, TexCoord2 + time);
+    outColor = mix(colTex1, colTex2, 0.5) * (colorEffect * vec4(Color, 1.0));
+}
 )glsl";
 
-int main() 
+int main (int argc, char *argv[]) 
 {
-    //Init, set up SDL_GL settings, window and context 
+    //Init, setup SDL_GL and GLEW
     SDL_Init(SDL_INIT_VIDEO);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     
-
-    SDL_Window* window = SDL_CreateWindow("Transformations with GLM", SDL_WINDOWPOS_CENTERED, 
-    SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_Window* window = SDL_CreateWindow("Further Transformations with GLM", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(window);
     
-
     glewExperimental = GL_TRUE;
     glewInit();
     printf("INITIALISING: OPENGL ERROR: %u\n", glGetError());
 
-    //create shader ints and compile/link shaders and program 
+    //create shader ints and compile, link and use shaderProgram
     GLint status;
     GLchar buffer[512];
-    
+
     GLuint vertexShader, fragmentShader, shaderProgram;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    vertexShader= glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
     glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
-    std::cout << "VERTEX SHADER COMPILE STATUS: " << status <<" VERTEX SHADER INFO LOG:\n" << buffer;
-    printf("VERTEX SHADER OPENGL ERROR: %u\n", glGetError());
+    std::cout << buffer << std::endl;
+    printf("COMPILING VERTEX SHADER ERROR: %u\n", glGetError());
     
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    fragmentShader= glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
     glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
     glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
-    std::cout << "FRAGMENT SHADER COMPILE STATUS: " << status << " FRAGMENT SHADER INFO LOG:\n" << buffer;
-    printf("FRAGMENT SHADER OPENGL ERROR: %u\n", glGetError());
+    std::cout << buffer << std::endl;
+    printf("COMPILING FRAGMENT SHADER ERROR: %u\n", glGetError());
     
+
     shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
+
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
     glGetProgramInfoLog(shaderProgram, 512, NULL, buffer);
-    std::cout << "SHADER PROGRAM LINK STATUS: " << status << " SHADER PROGRAM INFO LOG:\n" << buffer;
     glUseProgram(shaderProgram);
-    printf("SHADER PROGRAM OPENGL ERROR: %u\n", glGetError());
+
+    std::cout <<  status << buffer << std::endl;
     
-    //create vertex array objects, vertex buffer objects, element buffer objects, texture objects and data for all
-    GLuint vao, vbo, ebo, textures[2];
+    printf("LINKING SHADER PROGRAM ERROR: %u\n", glGetError());
+    //create object ints
+    GLuint vbo, ebo, vao, textures[2];
     glGenVertexArrays(1, &vao);
     //create data for objects
-    GLfloat vertices[] {
-        //Position      Color      TexCoords1   TexCoords2
-        -0.5, 0.5,  1.0, 0.0, 0.0,  0.0, 0.0,   0.0, 0.0,
-         0.5, 0.5,  1.0, 1.0, 0.0,  1.0, 0.0,   5.0, 0.0,
-        0.5, -0.5,  1.0, 1.0, 1.0,  1.0, 1.0,   5.0, 10.0,
-        -0.5, -0.5, 1.0, 1.0, 0.0,  0.0, 1.0,   0.0, 10.0
+    GLfloat vertices[] 
+    {   //position  //color         //texcoords
+        0.0, -0.75,     1.0, 1.0, 0.0,   0.5, -1.0,    2.5, -10.0,
+      -0.15, -0.25,     1.0, 0.5, 0.1,  0.25,  0.5,    0.0,  0.0, 
+       0.15, -0.25,   0.25, 0.25, 0.5,  0.75,  0.5,    3.75, 0.0,
+       -0.65, -0.25,    1.0, 0.0, 1.0,   0.0,  0.5,    0.0,  5.0,
+        0.65, -0.25,    0.0, 1.0, 0.0,   1.0,  0.5,    5.0,  5.0,
+      -0.25, 0.0,      1.0, 0.5, 1.0,  0.25,  0.0,    1.25, 0.0,
+       0.25, 0.0,    0.25, 0.5, 0.75, 0.75, 0.75,    3.75, 7.5,
+        0.0, 0.25,   0.75, 0.5, 0.25,  0.5, 0.75,    2.5,  7.5,
+        -0.45, 0.5,   0.5, 0.25, 0.75,  0.0,  1.0,    0.0,  10.0,
+         0.45, 0.5,   0.25, 0.75, 0.5,  1.0,  1.0,    5.0,  10.0
     };
 
-    GLuint indices[]
+    GLuint indices[] 
     {
-        0, 1, 3,
-        1, 2, 3
+        1, 0, 2,
+        3, 1, 5,
+        7, 5, 1,
+        1, 2, 7,
+        4, 2, 6,
+        7, 6, 2,
+        5, 8, 7,
+        7, 9, 6,
     };
-    
-    //bind and add data to array and buffer objects. Create and add data to texture objects
+
+    //bind to and write data to objects
     glBindVertexArray(vao);
-    
-    
+
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    printf("WRITNG TO VBO OPENGL ERROR: %u\n", glGetError());
-    
 
     glGenBuffers(1, &ebo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    printf("WRITING TO EBO OPENGL ERROR: %u\n", glGetError());
-    
-    
+
     glGenTextures(2, textures);
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textures[0]);
-    printf("BINDING TO TEXTURE1 OPENGL ERROR: %u\n", glGetError());
-    
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glBindTexture(GL_TEXTURE_2D, textures[0]);glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    printf("SETTING TEXTURE1 OPENGL ERROR: %u\n", glGetError());
     
     GLint width, height, nrChannels;
     unsigned char* data = stbi_load("Textures/container.jpg", &width, &height, &nrChannels, 0);
@@ -167,7 +175,6 @@ int main()
     }
     stbi_image_free(data);
     glUniform1i(glGetUniformLocation(shaderProgram, "tex1"), 0);
-    printf("WRITING TO TEXTURE1 OPENGL ERROR: %u\n", glGetError());
     
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, textures[1]);
@@ -175,7 +182,6 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    printf("SETTING UP TEXTURE2 OPENGL ERROR: %u\n", glGetError());
     
      data = stbi_load("Textures/cat.png", &width, &height, &nrChannels, 0);
     if(!data) 
@@ -189,10 +195,11 @@ int main()
     }
     stbi_image_free(data);
     glUniform1i(glGetUniformLocation(shaderProgram, "tex2"), 1);
-    printf("WRITING TO TEXTURE2 OPENGL ERROR: %u\n", glGetError());
     
-    //create and enable vertexAttrib for position, color and texcoords ("in" values of shader program)
     
+    GLint uniModel = glGetUniformLocation(shaderProgram, "model");
+    //creata and enable vertexAttrib pointers to ins in vertex Shader
+
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
     glEnableVertexAttribArray(posAttrib);
     glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (void*) 0);
@@ -216,12 +223,26 @@ int main()
     glVertexAttribPointer(texAttrib2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (void*) (7 * sizeof(GLfloat)));
     printf("TEXCOORD2 ATTRIB OPENGL ERROR: %u\n", glGetError()); 
     
-    //render loop
+    glm::mat4 view = glm::lookAt(
+        glm::vec3(1.2f, 1.2f, 1.2f),
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 0.0f, 1.0f)
+    );
+    GLint uniView = glGetUniformLocation(shaderProgram, "view");
+    glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
+    glm::mat4 proj = glm::perspective(
+        glm::radians(45.0f), 
+        800.0f/ 600.0f,
+        1.0f,
+        10.0f
+    );
+    GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
+    glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
 
-    printf("OPENGL ERROR: %u\n", glGetError());
+    //render loop
     SDL_Event windowEvent;
     while (true)
-    {
+    {    
         if (SDL_PollEvent(&windowEvent))
         {
             if (windowEvent.type == SDL_QUIT)
@@ -238,19 +259,33 @@ int main()
  
 
         float time = SDL_GetTicks()/ 1024.0f;
-        GLint uniTransform = glGetUniformLocation(shaderProgram, "transform");
-        glm::mat4 transform = glm::mat4(1.0f);
-        transform = glm::rotate(
-        transform, 
+        GLint uniModel = glGetUniformLocation(shaderProgram, "model");
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(
+        model, 
         time * glm::radians(90.0f), 
         glm::vec3(0.0f, 0.0f, 1.0f)
         );
+        glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
 
-        glUniformMatrix4fv(uniTransform, 1, GL_FALSE, glm::value_ptr(transform));
+        GLint colTrans = glGetUniformLocation(shaderProgram, "colorEffect");
+        glm::mat4 color = glm::mat4(1.0f);
+        color = glm::rotate (
+            color,
+            sin(time), 
+            glm::vec3(1.0, 1.0, 0.0)
+        );
+        glUniformMatrix4fv(colTrans, 1, GL_FALSE, glm::value_ptr(color));
         
-        
+        GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
+        glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
+
+        GLint uniView = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
+    
+         
         printf("TIME: %f\r", time);
-        glUniform1f(glGetUniformLocation(shaderProgram, "texWeight"), 0.5);
+        glUniform1f(glGetUniformLocation(shaderProgram, "time"), time);
         glUniform1i(glGetUniformLocation(shaderProgram, "tex1"), 0); 
         glUniform1i(glGetUniformLocation(shaderProgram, "tex2"), 1);
    
@@ -258,9 +293,10 @@ int main()
 
         glBindVertexArray(vao);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
 
         SDL_GL_SwapWindow(window);
+   
     }
     //delete shaders and shader program
     glDeleteShader(vertexShader);
@@ -276,10 +312,9 @@ int main()
     glDeleteBuffers(1, &ebo);
     glDeleteTextures(2, textures);
     glDeleteVertexArrays(1, &vao);
-
-    //quit and delete context
     
+    //delete context
     SDL_Quit();
     SDL_GL_DeleteContext(context);
-    return 0;
+    
 }
