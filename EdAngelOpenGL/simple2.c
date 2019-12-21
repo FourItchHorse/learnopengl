1ef634848fc4df41afecaa8b2d6dbd114dea3d13
Joseph Riley <joerileyart@gmail.com>
2019-12-21 19:23:10 +0000

    Finished first properly structured glut program

---
+++ EdAngelOpenGL/simple2.c
@@ -0,0 +1,38 @@
+#include <GL/freeglut.h>
+void display () 
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glBegin(GL_POLYGON);
+	  glVertex2f(-0.5, -0.5);
+          glVertex2f(-0.5, 0.5);
+          glVertex2f(0.5, 0.5);
+          glVertex2f(0.5, -0.5);
+	glEnd();
+
+	glFlush();	
+}		
+
+void init() 
+	
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	glColor3f(0.75, 0.75, 0.0);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("simple");
+	glutDisplayFunc(display);
+	init();
+	glutMainLoop();
+
+}
