ced4b4d4bdce35a5d9eff1489adb62aa38387941
Joseph Riley <joerileyart@gmail.com>
2020-01-11 15:33:50 +0000

    Swapped over to the opengl programming guide. Working better with this
    compared to Interactive Computer Graphics. Since this book seems to be
    packed with examples it's probably better to work my way through this
    then return to opengl a primer and interactive Computer Graphics
    afterwards as I am working with opengl 4.5.

---
+++ OGLProgGuide9/myIncl/vgl.h
@@ -0,0 +1,14 @@
+#ifndef __VGL_H__
+#define __VGL_H__
+
+
+#define GL3W_IMPLEMENTATION
+#include <GL/glew.h> 
+#include <GL/gl.h>
+
+
+#include <GLFW/glfw3.h>
+
+#define BUFFER_OFFSET(a)  ((void*)(a))
+
+#endif
