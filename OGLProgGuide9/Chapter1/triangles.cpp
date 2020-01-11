10db0aa8fc2c5a71663b6f37e8d40bd1a8a19820
Joseph Riley <joerileyart@gmail.com>
2020-01-11 15:31:14 +0000

    Swapped over to the Opengl programming guide. This
    really seems to be more what I need at the moment. Had a bit of a snag
    trying to get gl3w before replacing it with GLEW. Spent so much time
    writing out their shader reader by hand that I was ignoring stupid
    mistakes in my own code.

---
+++ OGLProgGuide9/Chapter1/triangles.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+#include "../MyIncl/vgl.h"
+#include "../MyIncl/LoadShaders.h"
+#include "../MyIncl/LoadShaders.cpp"
+enum VAO_IDs {Triangles, NumVAOs = 1};
+enum Buffer_IDs {ArrayBuffer, NumBuffers = 1};
+enum Attrib_IDs {vPosition = 0};
+
+GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
+
+const GLuint NumVertices = 6;
+
+void init(void) 
+{
+	static const GLfloat vertices[NumVertices][2] 
+	{
+		{-0.9, -0.9},
+		{0.85, -0.9},
+		{-0.9, 0.85},
+		{0.9, -0.85},
+		{0.9, 0.9},
+		{-0.85, 0.9}
+	};
+
+	glCreateVertexArrays(NumVAOs, VAOs);
+	glCreateBuffers(NumBuffers, Buffers);
+	glNamedBufferStorage(Buffers[ArrayBuffer], sizeof(vertices),
+			vertices, 0);
+	ShaderInfo shaders[] {
+		{GL_VERTEX_SHADER, "triangles.vert"},
+		{GL_FRAGMENT_SHADER, "triangles.frag"},
+		{GL_NONE, NULL}
+	};
+
+	GLuint program = ShaderReader::LoadShaders(shaders);
+	glUseProgram(program);
+
+	glBindVertexArray(VAOs[Triangles]);
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 
+			0, BUFFER_OFFSET(0));
+	glEnableVertexAttribArray(vPosition);
+}
+void display(void) 
+{
+	static const float black[] = {0.0f, 0.0f, 0.0f, 0.0f};
+	glClearBufferfv(GL_COLOR, 0, black);
+
+	glBindVertexArray(VAOs[Triangles]);
+	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+}
+int main (int argc, char** argv) 
+{
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Triangles", 
+			NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glewInit();
+	
+	init();
+
+	while(!glfwWindowShouldClose(window)) 
+	{
+		display();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
