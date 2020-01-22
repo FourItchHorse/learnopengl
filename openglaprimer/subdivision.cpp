a65cc7f849ccd672d8e68ec920045b6202687251
Joseph Riley <joerileyart@gmail.com>
2020-01-22 13:00:25 +0000

    Created subdividing triangle

--- openglaprimer/subdivision.cpp
+++ openglaprimer/subdivision.cpp
@@ -1,4 +1,6 @@
 #include <GL/glut.h>
+       GLfloat v[3][2] = {0.0, 0.75, -0.75, -0.75, 0.75, -0.75};
+       int n = 2;
 void triangle(GLfloat *a, GLfloat *b, GLfloat *c) 
 {
 	glBegin(GL_TRIANGLES);
@@ -8,11 +10,30 @@ void triangle(GLfloat *a, GLfloat *b, GLfloat *c)
 	glEnd();
 
 }
+void divide_triangle (GLfloat *a, GLfloat *b, GLfloat *c, int m) 
+{
+	GLfloat v[3][2];
+	int j;
+	if(m>0) 
+	{
+		for(j = 0; j < 2; j++) v[0][j] = (a[j] + b[j])/2;
+		for(j = 0; j < 2; j++) v[1][j] = (a[j] + c[j])/2;
+		for(j = 0; j < 2; j++) v[2][j] = (b[j] + c[j])/2;
+		divide_triangle(a, v[0], v[1], m - 1);
+		divide_triangle(v[0], b, v[2], m - 1);
+		divide_triangle(v[1], v[2], c, m - 1);
+		divide_triangle(v[0], v[1], v[2], m - 1);
+	}
+	else 
+	{
+		triangle(a, b, c);
+	}
+}
 void display() 
 {
    glClear(GL_COLOR_BUFFER_BIT);
 	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	triangle({0.0, 0.0}, {0.5, 1.0}, {1.0, 1.0});
+	divide_triangle(v[0], v[1], v[2], n);
    glFlush();
 }
 void init() 
@@ -23,6 +44,7 @@ void init()
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluOrtho2D( -1.0, 1.0, -1.0, 1.0);
+
 }
 int main(int argc, char** argv) 
 {
