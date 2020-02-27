cedb1d96ee8be2f049fb0ce102d506fb1676d597
Joseph Riley <joerileyart@gmail.com>
2020-02-27 14:09:43 +0000

    Developing OGL/DeVries hybrid program a bit more

--- JoeyDeVriesMarch2020/OGLProgGuideTriangle.cpp
+++ JoeyDeVriesMarch2020/OGLProgGuideTriangle.cpp
@@ -5,8 +5,8 @@
 #include <cstdlib>
 #include <iostream>
 enum VAO_IDs { Triangles, NumVAOs };
-enum Buffer_IDs { ArrayBuffer, NumBuffers };
-enum Attrib_IDs { vPosition = 0 };
+enum Buffer_IDs { ArrayBuffer, ElementBuffer, NumBuffers };
+enum Attrib_IDs { vPosition = 0, vColour = 1};
 
 GLuint VAOs[NumVAOs];
 GLuint Buffers[NumBuffers];
@@ -19,6 +19,7 @@ typedef struct {
 	GLuint 	      shader;
 } ShaderInfo;
 
+	/*code from github.com/openglredbook/examples/blob/master/lib/LoadShaders.cpp*/
 static const GLchar* ReadShader(const char* filename) 
 {
 	FILE* infile = fopen (filename, "rb" );
@@ -45,38 +46,124 @@ static const GLchar* ReadShader(const char* filename)
 }
 GLuint LoadShaders(ShaderInfo* shaders) 
 {
-	/*github.com/openglredbook/examples/blob/master/lib/LoadShaders.cpp*/
 
 	if( shaders == NULL ) {return 0; }
 
 	GLuint program = glCreateProgram();
 
 	ShaderInfo* entry = shaders;
+
+	while( entry->type != GL_NONE) 
+	{
+		GLuint shader = glCreateShader(entry->type);
+
+		entry->shader = shader;
+
+		const GLchar* source = ReadShader(entry-> filename);
+		if ( source  == NULL ) {
+		for(entry = shaders; entry->type != GL_NONE; ++entry) {
+				glDeleteShader( entry->shader );
+				entry->shader = 0;
+			}
+
+
+			return 0;
+		}
 	
+
+
+	glShaderSource(shader, 1, &source, NULL);
+	delete [] source;
+
+	glCompileShader(shader);
+
+	GLint compiled;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled );
+	if (! compiled ) {
+		#ifdef _DEBUG
+			GLsizei len;
+			GLchar* log = new GLchar[len+1];
+			glGetShaderInfoLog(shader, len, &len, log)
+				std::cerr << "Shader compilation failed :" << log << std::endl;
+			delete [] log;
+		#endif
+
+			return 0;
+	}
+
+	
+	glAttachShader( program, shader);
+
+	++entry
+	}
+
+	glLinkProgram( program );
+
+	GLint linked;
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+	if(!linked) 
+	{
+	#ifdef _DEBUG
+		GLsizei len;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len );
+
+
+		GLchar* log = new GLchar[len +1]
+		glGetProgramInfoLog(program, len, &len, log);
+		std::cerr << "Shader linking failed: " << log << std::endl;
+		delete[] log;
+	#endif
+	
+	
+	for(entry = shaders; entry->type != GL_NONE; ++entry) 
+	{
+		glDeleteShader(entry->shader);
+		entry->shader = 0;
+	}
+		return 0;
+	}
+
+	return program;
 }
+/* END OF SHADER COMPILER*/
 
 void init (void) 
 {
-	static const GLfloat vertices[NumVertices][2] =
+	static const GLfloat vertices[NumVertices][5] =
 	{
-		{-0.9, -0.9},
-		{0.85, -0.9},
-		{-0.9, 0.85},
+		{-0.9, -0.9, 1.0, 1.0, 1.0},
+		{0.85, -0.9, 1.0, 0.0, 0.0},
+		{-0.9, 0.85, 0.0, 1.0, 0.0},
 		
-		{0.9, -0.85},
-		{0.9, 0.9},
-		{-0.85, 0.9}
+		{0.9, -0.85, 0.0, 0.0, 1.0},
+		{0.9, 0.9,   0.0, 1.0, 0.0},
+		{-0.85, 0.9, 1.0, 0.0, 0.0}
 	};
 
+
+	glCreateVertexArrays(NumVAOs, VAOs);
+
 	glCreateBuffers(NumBuffers, Buffers);
-	glNamedBufferStorage(Buffers[ArrayBuffer],
-		sizeof(vertices), 0);
+	glNamedBufferStorage(Buffers[ArrayBuffer], sizeof(vertices), 0);
 
 	ShaderInfo shaders[] = {
 		{GL_VERTEX_SHADER, "27022020.vert" },
 		{GL_FRAGMENT_SHADER, "27022020.frag" },
 		{GL_NONE, NULL }
 	};
+
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);
+
+	glBindVertexArray(VAOs[Triangles]);
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	
+	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(vPosition);
+
+	glVertexAttribPointer(vColour, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)2);
+	glEnableVertexAttribArray(vColour);
+
 }
 void display(void)
 {
