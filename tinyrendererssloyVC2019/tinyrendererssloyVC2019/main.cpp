6508cd89e2fae7069a3944290ee5ed0d78011a01
Joseph Riley <joerileyart@gmail.com>
2020-03-12 10:28:55 +0000

    Managed to tinyrender a wireframe
    but it didn't go too well. Must be something wrong with my line
    algorithm. Nevertheless, this is a significant step since I managed
    to re-write code that converted an .obj file to an array of vertices
    and indices which will be useful later.

--- tinyrendererssloyVC2019/tinyrendererssloyVC2019/main.cpp
+++ tinyrendererssloyVC2019/tinyrendererssloyVC2019/main.cpp
@@ -1,8 +1,13 @@
 #include "tgaimage.h"
+#include "model.h"
 #include <iostream>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+Model* model = NULL;
+const int width = 800;
+const int height = 800;
+
 void line(int x0, int x1, int y0, int y1, TGAImage &image, TGAColor color) 
 {
 	bool steep = false;
@@ -11,43 +16,73 @@ void line(int x0, int x1, int y0, int y1, TGAImage &image, TGAColor color)
 		std::swap(x0, y0);
 		std::swap(x1, y1);
 	}
-	int D, dx, dy;
-	dy = y1 - y0;
-	int yi = 1;
-	if (dy < 0) {
-		yi = -1;
-		dy = -dy;
-	}
-	if (x0 > x1)
+	if (x0 > x1) {
 		std::swap(x0, x1);
-	dx = x1 - x0;
-	D = (2 * dy) - dx; 
+		std::swap(y0, y1);
+	}
+	int dy = y1 - y0;
+	int dx = x1 - x0;
+	int D = std::abs(dy)*2 - dx; 
 	int y = y0;
-	
-	for (int x = x0; x <= x1; x += 1)
+
+#ifndef __DEBUG
+	std::cerr << std::endl;
+#endif
+
+	if (steep) 
+		for (int x = x0; x <= x1; x += 1)
+		{
+			image.set(y, x, color); 
+			
+			if (D > 0) {
+				y += (y1 > y0 ? 1 : -1);
+				D -= 2 * dx;
+			}
+			D += 2 * dy;
+		}
+	else for (int x = x0; x <= x1; x += 1)
 	{
-		if (steep)
-			image.set(y, x, color);	
-		else
 			image.set(x, y, color);
 		if (D > 0) {
-			y += yi;
+			y += (y1 > y0?1:-1);
 			D -= 2 * dx;
 		}
 		D +=  2*dy; 
 	}
+
+#ifndef __DEBUG
+	std::cerr << std::endl;
+#endif
 }
 
 int main(int argc, char** argv) 
 {
 
-	TGAImage image(100, 100, TGAImage::RGB);
-	line(20, 13, 40, 80, image, red);
-	line(80, 40, 13, 20, image, red); 
-	line(13, 20, 80, 40, image, white);
-	line(0, 100, 0, 100, image, white);
+	TGAImage image(width, height, TGAImage::RGB);
+	if (2 == argc)
+	{
+		model = new Model(argv[1]);
+	}
+	else
+	{
+		model = new Model("obj/sphere.obj");
+	}
+	for (int i = 0; i < model->nfaces(); i++)
+	{
+		std::vector<int> face = model->face(i);
+		for  (int j = 0; j < 3;  j++)
+		{
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j+1)%3]);
+			int x0 = (v0.x + 1)*width/2;
+			int y0 = (v0.y + 1)*height/2; 
+			int x1 = (v1.x + 1)*width /2;
+			int y1 = (v1.y + 1)*height/2;
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
 	image.flip_vertically();
-	image.write_tga_file("Debug/10032020lines.tga");
+	image.write_tga_file("Debug/12032020wireframe.tga");
 	return 0;
 }
 
